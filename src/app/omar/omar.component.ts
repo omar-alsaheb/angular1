896bb923d1d797082b7911417bf696fc846768b0
Omar AL Tamimi <OmarT@leading-point.com>
2022-05-19 15:26:00 +0300

    sections

--- src/app/omar/omar.component.ts
+++ src/app/omar/omar.component.ts
@@ -1,8 +1,9 @@
 import { HttpClient } from '@angular/common/http';
 import { Component, OnDestroy, OnInit } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
-import { debounceTime, Subscription } from 'rxjs';
-// import { CustomValidators } from './custome-validators';
+import { Subscription } from 'rxjs';
+import { MatDialog } from '@angular/material/dialog';
+
 @Component({
   selector: 'app-omar',
   templateUrl: './omar.component.html',
@@ -10,35 +11,40 @@ import { debounceTime, Subscription } from 'rxjs';
 })
 export class OmarComponent implements OnInit, OnDestroy {
   subsciption: Subscription;
+  hide = true;
   signupForm: FormGroup = new FormGroup({
-    Account_ID: new FormControl(null, [Validators.required, Validators.minLength(5)]),
+    Account_ID: new FormControl(null, [
+      Validators.required,
+      Validators.minLength(5),
+    ]),
     Password: new FormControl(null, [Validators.required]),
     IsMd5Password: new FormControl(false),
     IsAutoConvertMd5: new FormControl(true),
   });
-  constructor(public http: HttpClient) {}
-
-  ngOnInit(): void {
-
+  constructor(public http: HttpClient, public dialog: MatDialog) {}
+  openDialog() {
+    this.dialog.open(DialogElementsExampleDialog);
   }
+  
+  ngOnInit(): void {}
 
   ngOnDestroy(): void {
     this.subsciption.unsubscribe();
   }
-
 
   createAccount() {
     if (this.signupForm.invalid) {
       alert('UserName or password is empty');
     } else {
       this.http
-        .post('https://api.alsaheb.xyz/api/Account/AddNewAccount', {
+        .post('https://api.romoldschool.uk/api/Account/AddNewAccount', {
           ...this.signupForm.value,
           IsMd5Password: false,
           IsAutoConvertMd5: true,
         })
         .subscribe(
           (res: any) => {
+            // this.messages = res.error;
             this.signupForm.reset();
             if (res.error) {
               // console.log(res);
@@ -54,4 +60,11 @@ export class OmarComponent implements OnInit, OnDestroy {
         );
     }
   }
+}
+@Component({
+  selector: 'dialog-elements-example-dialog',
+  templateUrl: 'dialog-elements-example-dialog.html',
+})
+export class DialogElementsExampleDialog {
+   messages: string;
 }
